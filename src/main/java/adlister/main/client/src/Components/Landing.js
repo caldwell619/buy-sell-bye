3a2f879b82d52a7818b0f2e4b647ceab7bf13247
admin <chaseohm@gmail.com>
2019-02-14 15:10:21 -0600

    finished profile mobile

--- src/main/java/adlister/main/client/src/Components/Landing.js
+++ src/main/java/adlister/main/client/src/Components/Landing.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-class Landning extends Component {
+class Landing extends Component {
     render() {
         return (
             <React.Fragment>
+                <input id="toggle" type="checkbox"/>
+                <label className="toggle-container" htmlFor="toggle">
+                    <span className="button button-toggle"></span>
+                </label>
+
+                <div className="device">
+                    <div className="container">
+                        <button id="burger" className="open-main-nav" >
+                            <span className="burger"></span>
+                            <span className="burger-text">Menu</span>
+                        </button>
+                        <nav className="main-nav" id="main-nav">
+                            <ul>
+                                <li><Link to="/profile/listings">Profile</Link></li>
+                                <li><Link to="/postings">Postings </Link></li>
+                                <li><Link to="/contact">Contact </Link></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
                 <div className="hero-image">
                     <div className="hero-text">
                         <h1>Buy..Sell...Bye</h1>
@@ -14,29 +34,10 @@ class Landning extends Component {
                     </div>
                 </div>
 
-                <input id="toggle" type="checkbox"/>
-                    <label className="toggle-container" htmlFor="toggle">
-                        <span className="button button-toggle"></span>
-                    </label>
 
-                    <div className="device">
-                        <div className="container">
-                            <button id="burger" className="open-main-nav">
-                                <span className="burger"></span>
-                                <span className="burger-text">Menu</span>
-                            </button>
-                            <nav className="main-nav" id="main-nav">
-                                <ul>
-                                    <li><Link to="/profile">Profile</Link></li>
-                                    <li><Link to="/postings">Postings </Link></li>
-                                    <li><Link to="/contact">Contact </Link></li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
             </React.Fragment>
         )
     }
 }
 
-export default Landning;
+export default Landing;
