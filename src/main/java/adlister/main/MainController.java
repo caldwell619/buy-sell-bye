05e01002c4d36d634d1784d9213f3a2bce85354f
caldwell619 <caldwell8814@gmail.com>
2019-03-06 13:33:52 -0600

    help

--- src/main/java/adlister/main/MainController.java
+++ src/main/java/adlister/main/MainController.java
@@ -3,6 +3,7 @@ package adlister.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class MainController {
     public static void main(String[] args) {
